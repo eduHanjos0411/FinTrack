a824844b8d38b9b31c540f0f97e7d2c7050cf2bf
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-02-13 11:54:34 -0300

    feat: adding login, register and dashboard routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home";
+import { Login } from "../pages/Login";
+import { Register } from "../pages/Register";
+import { Dashboard } from "../pages/Dashboard";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/registro" element={<Register/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
     </Routes>
   )
 }
