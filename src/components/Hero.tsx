b7916a45c1ea870ba8a830e1cb64af57dee6f483
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-02-13 11:54:17 -0300

    feat: adding hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,10 @@
+import '../styles/hero.css'
+
 export function Hero() {
   return (
     <section className="hero">
-      <h1>FinTrack</h1>
-      <h2>Seu sistema de controle financeiro descomplicado</h2>
+      <h1 className='title-hero'><p className='first'>Fin</p><p className='second'>Track</p></h1>
+      <h2 className='subtitle-hero'>Seu sistema de controle financeiro descomplicado</h2>
     </section>
   )
 }
