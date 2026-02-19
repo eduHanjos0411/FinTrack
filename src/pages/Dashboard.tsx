0d585d3a5094f1fd011d858bf59e2d64079850a3
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-02-19 11:52:02 -0300

    feat: adding components to display on dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,21 @@
+import { CardDespesas } from "../components/cards/CardDespesas";
+import { CardInvestimentos } from "../components/cards/CardInvestimentos";
+import { CardSaldo } from "../components/cards/CardSaldo";
+import { TopHeader } from "../components/TopHeader";
+
+import "../styles/dashboard.css";
+
 export function Dashboard() {
   return (
     <div className="container-dashboard">
-      
+      <header className="header-dashboard">
+        <TopHeader />
+      </header>
+      <div className="cards-container">
+        <CardSaldo />
+        <CardDespesas />
+        <CardInvestimentos />
+      </div>
     </div>
-  )
+  );
 }
