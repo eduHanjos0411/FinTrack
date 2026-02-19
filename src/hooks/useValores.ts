7a1f753cae25139d5d7c1b13f27c241e5ad9aed2
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-02-19 12:22:08 -0300

    feat: to finish, creating hook to add new values with modal

---
+++ src/hooks/useValores.ts
@@ -0,0 +1,13 @@
+import { useMemo, useState } from "react";
+import type { Valor } from "../types/valor";
+import { sumBy } from "../utils/sumBy";
+
+export function useValores(initalValues: Valor[]) {
+  const [valores, setValores] = useState<Valor[]>(initalValues)
+
+  const total =  sumBy(valores, (item) => item.valor)
+
+  function addValor(novoValor: Valor) {
+    setValores((prev) => [...prev, novoValor])
+  }
+}
