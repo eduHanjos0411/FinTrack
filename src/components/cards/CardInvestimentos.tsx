1c6afe503a308c71cfa7a78227e65495b7316a39
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-02-20 10:09:01 -0300

    refactor: refining componentization and adding context

--- src/components/cards/CardInvestimentos.tsx
+++ src/components/cards/CardInvestimentos.tsx
@@ -1,51 +1,18 @@
-import type { Valor } from "../../types/valor";
-import { formatCurrency } from "../../utils/formatCurrency";
-import { sumBy } from "../../utils/sumBy";
+import { useFinance } from "../../contexts/FinanceContext";
 import { CardTable } from "../CardTable";
+import { TotalDisplay } from "../TotalDisplay";
 import { Card } from "./Card";
 
-const investimentos: Valor[] = [
-  {
-    nome: "Reserva",
-    valor: 200,
-  },
-  {
-    nome: "CDB",
-    valor: 150,
-  },
-  {
-    nome: "FII",
-    valor: 300,
-  },
-];
-
-const totalValue = sumBy(investimentos, (i) => i.valor);
-
-const total = (
-  <div className="card-content">
-    Total Investido:
-    <p className="card-investimento-text">{formatCurrency(totalValue)}</p>
-  </div>
-);
-
-const content = (
-  <CardTable>
-    {investimentos.map((investimento) => (
-      <tr key={investimento.nome}>
-        <td>{investimento.nome}</td>
-        <td className="td-valor">{formatCurrency(investimento.valor)}</td>
-      </tr>
-    ))}
-  </CardTable>
-);
-
 export function CardInvestimentos() {
+const { investimentos } = useFinance()
+
   return (
     <Card
       title="Total Investido"
       subtitle="Adicione seus investimentos"
-      total={total}
-      content={content}
+      children="Últimas Transações"
+      content={<CardTable colorValue="card-saldo-text" valueType="+" items={investimentos.valores} />}
+      total={<TotalDisplay textClassName="card-investimento-text" displayType="Investido" value={investimentos.total}/>}
     />
   );
 }
