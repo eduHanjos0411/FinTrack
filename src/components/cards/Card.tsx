c1e668cc5022ee445c456e34cbcc8bd8aa522a5c
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-02-20 10:08:50 -0300

    refactor: improving component reusability

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -1,29 +1,31 @@
 import type React from "react";
 
-import '../../styles/card.css'
-import { AddValueModal } from "../AddSaldoModal";
-
+import "../../styles/card.css";
 interface cardProps {
-  title: string;
-  subtitle: string;
-  total: React.ReactNode;
-  content: React.ReactNode;
+  title: string
+  subtitle: string
+  total: React.ReactNode
+  content?: React.ReactNode
+  children?: React.ReactNode
 }
 
-export function Card({ title, subtitle, total, content }: cardProps) {
+export function Card({ title, subtitle, total, content, children }: cardProps) {
   return (
     <div className="card-container">
       <div className="card-top">
-      <h3 className="card-title">{title}</h3>
-      <h4 className="card-subtitle">{subtitle}</h4>
-      <div className="card-total">{total}</div>
-      <button className="btn-card" onClick={AddValueModal}>Adicionar</button>
-      <hr />
-      <div className="card-content">{content}</div>
+        <h3 className="card-title">{title}</h3>
+        <h4 className="card-subtitle">{subtitle}</h4>
+        <div className="card-total">{total}</div>
+        <div className="card-actions">
+          <button className="btn-card">Adicionar</button>
+          <button className="btn-card">Ver detalhes</button>
+        </div>
+        <hr />
+        <div className="card-bottom">
+          <p>{children}</p>
+          <div className="card-content">{content}</div>
+        </div>
       </div>
-      <ul className="card-list">
-        <li className="card-list-item"></li>
-      </ul>
     </div>
-  )
+  );
 }
